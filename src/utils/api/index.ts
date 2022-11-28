ae20c5824099d05115e7bf766ff8d377544cb194
hawkf07 <hawkf07@proton.me>
2022-11-28 13:40:20 +0700

    feat: adding api functions

---
+++ src/utils/api/index.ts
@@ -0,0 +1,13 @@
+type getApiDataFromNasaApiBySearchType = (q: string) => Promise<void>;
+
+export const getApiDataFromNasaApiBySearch = async (q: string) => {
+  const response = await fetch(`https://images-api.nasa.gov/search?q={q}`);
+  const data = await response.json();
+  return data;
+};
+
+export const getApiData = async (url: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
